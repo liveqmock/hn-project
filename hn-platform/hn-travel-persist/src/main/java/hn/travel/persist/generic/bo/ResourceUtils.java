a89244ded311d672e397e84e446fe86e32f349fd
chenbing <chenbing@user-PC>
2014-08-24 21:04:03 +0800

    添加注解
    Signed-off-by: chenbing <chenbing@user-PC>

--- hn-platform/hn-travel-persist/src/main/java/hn/travel/persist/generic/bo/ResourceUtils.java
+++ hn-platform/hn-travel-persist/src/main/java/hn/travel/persist/generic/bo/ResourceUtils.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 /**
  * @author chenbing
- *
+ * 
  */
 public class ResourceUtils {
-	
+
 	private static List<Resource> rs;
-	
+	/** 根节点ID */
 	public static final Long ROOT_ID = 0L;
 
 	static {
@@ -25,21 +25,21 @@ public class ResourceUtils {
 		rs.add(new Resource(5L, "会员管理_会员列表", "/mem", 1, 1, 2L));
 		rs.add(new Resource(6L, "酒店管理_酒店列表", "/hotel", 1, 1, 3L));
 	}
-	
+
 	public static List<Resource> getByParentId(Long pid) {
 		List<Resource> list = new ArrayList<>();
-		for(Resource r : rs) {
-			if(r.getParentId().compareTo(pid) == 0) {
+		for (Resource r : rs) {
+			if (r.getParentId().compareTo(pid) == 0) {
 				list.add(r);
 			}
 		}
 		return list;
 	}
-	
+
 	public static List<Resource> getTopMenus() {
 		List<Resource> list = new ArrayList<>();
-		for(Resource r : rs) {
-			if(r.getParentId().compareTo(ROOT_ID) == 0) {
+		for (Resource r : rs) {
+			if (r.getParentId().compareTo(ROOT_ID) == 0) {
 				list.add(r);
 			}
 		}
