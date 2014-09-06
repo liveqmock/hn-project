6e8a1e609c09d475b899e2842eb67c2674a42c8d
yangyang <2775813138@qq.com>
2014-09-06 18:28:34 +0800

    sql

--- hn-platform/hn-travel-persist/src/main/resources/sql/mysql/schema.sql
+++ hn-platform/hn-travel-persist/src/main/resources/sql/mysql/schema.sql
@@ -19,3 +19,131 @@ create table ss_user (
 	register_date timestamp not null default 0,
 	primary key (id)
 ) engine=InnoDB;
+
+-- ----------------------------
+-- Table structure for favorites
+-- ----------------------------
+DROP TABLE IF EXISTS `favorites`;
+CREATE TABLE `favorites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mem_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `state` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for item
+-- ----------------------------
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(50) DEFAULT NULL,
+  `item_type` varchar(50) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `price_sale` double DEFAULT NULL,
+  `item_desc` varchar(500) DEFAULT NULL,
+  `picture` varchar(200) DEFAULT NULL,
+  `state` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for member
+-- ----------------------------
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mobile` varchar(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `salt` varchar(20) DEFAULT NULL COMMENT '盐值',
+  `addr` varchar(100) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `state` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='保存会员信息';
+
+-- ----------------------------
+-- Table structure for order_addr
+-- ----------------------------
+DROP TABLE IF EXISTS `order_addr`;
+CREATE TABLE `order_addr` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mem_id` int(11) DEFAULT NULL,
+  `consignee_name` varchar(20) DEFAULT NULL,
+  `addr` varchar(200) DEFAULT NULL,
+  `mobile` char(11) DEFAULT NULL,
+  `phone` char(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `postcode` char(6) DEFAULT NULL,
+  `best_time` varchar(20) DEFAULT NULL,
+  `landmark` varchar(20) DEFAULT NULL,
+  `state` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_id` (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for order_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE `order_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `state` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mem_id` int(11) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `order_addr_id` int(11) DEFAULT NULL,
+  `order_msg` varchar(500) DEFAULT NULL,
+  `express_id` varchar(50) DEFAULT NULL,
+  `state` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for shopping
+-- ----------------------------
+DROP TABLE IF EXISTS `shopping`;
+CREATE TABLE `shopping` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mem_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `state` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
